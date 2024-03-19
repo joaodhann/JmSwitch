40d8ceb5ebb21f9511c4b34d553ee4317ad3e1d0
Usuario <Usuario@CPD02-M05>
2024-03-19 10:43:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JmSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+
+            double um = double.Parse(Console.ReadLine());    
+
+            Console.WriteLine("Digite o segundo número");
+
+            double dois = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a operação");
+
+            string operacao = Console.ReadLine().ToUpper();   
+
+            switch (operacao)
+            {
+                case "+":
+                     double resultado = um + dois;
+                     Console.WriteLine("O resultado é: " + resultado); 
+                        break;
+                case "-":
+                    double resultado2 = um - dois;
+                    Console.WriteLine("O resultado é: " + resultado2);
+                    break;
+                case "*":
+                    double resultado3 = um * dois;
+                    Console.WriteLine("O resultado é:" + resultado3);
+                    break;
+                case "/":
+                    double resultado4 = um / dois;
+                    Console.WriteLine("O resultado é: " + resultado4);
+                    break;
+                default:
+                    Console.WriteLine("ERRO, OPÇÃO INVÁLIDA");
+                    break;
+            }
+               Console.ReadKey();
+            
+        }
+    }
+}
